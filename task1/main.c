3dc547c88f3d0d5662b6eb7dd9e226c4911d82a6
YURII <yurii@YURIIs-iMac.local>
2015-02-10 10:48:05 +0200

    Initial Commit

---
+++ task1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  task1
+//
+//  Created by YURII on 10.02.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
